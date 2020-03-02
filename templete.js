36a65f913e04887ee1bf9bee3825093d90f2450c
amirul-bd <mdamirul655@gmail.com>
2020-03-03 01:17:11 +0600

    add templete

---
+++ templete.js
@@ -0,0 +1,14 @@
+const fName = "Amirul";
+const lName = "Islam";
+const fullName = fName + lName;
+
+const fullName2 = `${fName} ${lName}`
+
+const total = `amer sonar bangla 
+ami tomai valo pai`
+
+
+
+console.log(total);
+
+
