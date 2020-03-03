885b4fcf9341608723c1196ea69d8e017727e36b
amirul-bd <mdamirul655@gmail.com>
2020-03-03 14:33:00 +0600

    add arrow

---
+++ arrow.js
@@ -0,0 +1,25 @@
+// function doubleIt(num) {
+//     return num * 2;
+// }
+
+// const doubleIt = function myFun(num){
+//     return num * 2;
+// }
+
+// const doubleIt = (num) => num * 3;
+// const add = (x , y) => x + y;
+// const give = () => 5;
+
+const doMath = (x, y) => {
+    const sum = x + y;
+    const diff = x - y;
+    const result = sum * diff;
+    return result;
+}
+
+
+
+//const results = add(25, 5);
+//const giveResult = give();
+const reTotal = doMath(5, 3);
+console.log(reTotal);
