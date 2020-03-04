84efac53857a450a648ecf4b35855cc8c0267e51
amirul-bd <mdamirul655@gmail.com>
2020-03-04 20:33:24 +0600

    add default

---
+++ letcons.js
@@ -0,0 +1,27 @@
+const actor = "Tanjin Tisa";
+console.log(actor);
+
+
+const number = [12, 45];
+number[0] = 88;
+number.push[12];
+console.log(number);
+
+
+const nayok = {name: "siaam ", phone: "017**-**** "};
+console.log(nayok);
+
+
+let patientName = "Rahim";
+patientName = "rafat";
+console.log(patientName);
+
+// let dile lik hoi na and var dile lik hoi
+// let / var
+let sum = 0;
+for (var i = 0; i < 10; i++) {
+    const sim = sum + i;
+    
+}
+console.log(i);
+
