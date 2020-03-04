9a85c07fdf6d9c0189e0e178959ed62cf35ad5fa
amirul-bd <mdamirul655@gmail.com>
2020-03-04 20:50:59 +0600

    modifide templete

--- templete.js
+++ templete.js
@@ -4,11 +4,18 @@ const fullName = fName + lName;
 
 const fullName2 = `${fName} ${lName}`
 
+
+ 
+const multiLine = 
+"i like you"
++ "I miss you"
++ "I need You"
+
 const total = `amer sonar bangla 
 ami tomai valo pai`
 
 
 
-console.log(total);
+console.log(multiLine);
 
 
